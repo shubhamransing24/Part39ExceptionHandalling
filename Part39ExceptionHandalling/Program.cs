a80220a63f6d50f3908ab9ec85059366a2bff13f
priyasonar1207 <83229578+priyasonar1207@users.noreply.github.com>
2021-10-07 16:23:11 +0530

    Add project files.

---
+++ Part39ExceptionHandalling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Part39ExceptionHandalling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = null;
+         try
+            {
+                reader = new StreamReader("E:\\New folder\\data.txt");
+                Console.WriteLine(reader.ReadToEnd());
+               
+
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                if(reader!=null)
+                {
+                    reader.Close();
+                }
+
+                Console.WriteLine("Finally Block always Executes");
+            }
+
+        }
+    }
+}
